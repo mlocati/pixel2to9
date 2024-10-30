e501ecbb45b21c77aebfd07328133ed8bb7ca684
Michele Locati <michele@locati.it>
2024-10-30 15:09:57 +0100

    Fix error in WhaleChart converter

--- src/Conversion/Converter/Block/WhaleChart.ts
+++ src/Conversion/Converter/Block/WhaleChart.ts
@@ -5,7 +5,7 @@ export default class WhaleChart extends Block implements Converter
 {
     public convert(doc: XMLDocument): void
     {
-        this.renameBlockTypeHandle(doc, 'whale_chart', 'pixel_chart');
+        this.renameBlockTypeHandle(doc, 'whale_chart', 'pixel_pie_chart');
         this.removeDataFields(doc, 'btWhaleChart', ['lineCap', 'scaleColor']);
         this.renameDataTable(doc, 'btWhaleChart', 'btPixelPieChart');
     }
