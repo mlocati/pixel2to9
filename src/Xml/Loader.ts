40df217c623601e86efbef4ef3323129714d8423
Michele Locati <michele@locati.it>
2024-10-30 10:22:13 +0100

    Fix detecting errors while loading XML

--- src/Xml/Loader.ts
+++ src/Xml/Loader.ts
@@ -3,8 +3,9 @@ const parser = new DOMParser();
 export default function loadXML(xml: string): XMLDocument
 {
   const doc = parser.parseFromString(xml, "application/xml");
-  if (doc.documentElement.nodeName === "parsererror") {
-    throw new Error(doc.documentElement.textContent || "Error parsing XML");
+  const errorElement = doc.querySelector('parsererror');
+  if (errorElement) {
+    throw new Error(errorElement.innerHTML)
   }
   if (doc.documentElement.tagName !== "concrete5-cif") {
     throw new Error("It seems the provided XML is not a Concrete CIF file");
