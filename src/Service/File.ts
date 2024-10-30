2a10242b663ff3a67c79a26e3e325ab3a8bfbb16
Michele Locati <michele@locati.it>
2024-10-30 14:37:50 +0100

    Fix handling file reader errors

--- src/Service/File.ts
+++ src/Service/File.ts
@@ -3,7 +3,7 @@ export function readFile(file: File): Promise<string>
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
         reader.addEventListener('load', () => resolve(<string>reader.result));
-        reader.addEventListener('error', (e: any) => reject(e?.message || e?.toString() || '?'));
+        reader.addEventListener('error', () => reject(reader?.error?.message || 'Failed to load file'));
         reader.readAsText(file);
     });
 }
