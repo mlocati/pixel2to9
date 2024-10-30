b2ab8e76cb1337aa9bdfd146bdbd9bc64b95467a
Michele Locati <michele@locati.it>
2024-10-30 11:30:17 +0100

    Fix readFile

--- src/Service/File.ts
+++ src/Service/File.ts
@@ -2,7 +2,7 @@ export function readFile(file: File): Promise<string>
 {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
-        reader.addEventListener('load', () => resolve(reader.result));
+        reader.addEventListener('load', () => resolve(<string>reader.result));
         reader.addEventListener('error', (e: any) => reject(e?.message || e?.toString() || '?'));
         reader.readAsText(file);
     });
