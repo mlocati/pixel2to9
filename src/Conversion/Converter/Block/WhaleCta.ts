5ee710722ad7604cb924361c85ad982f9c29191d
Michele Locati <michele@locati.it>
2024-10-30 15:10:36 +0100

    Minor change

--- src/Conversion/Converter/Block/WhaleCta.ts
+++ src/Conversion/Converter/Block/WhaleCta.ts
@@ -10,10 +10,10 @@ export default class WhaleCta extends Block implements Converter
             pixel_btn_3d: {newTemplate: 'pixel_button', newCustomClasses: 'button:style:3d'},
             pixel_btn_simple_right: {newTemplate: 'pixel_button', newCustomClasses: 'utl:text:align:end'},
         });
-        this.renameBlockTypeHandle(doc, 'whale_cta', 'pixel_cta');
-        this.renameDataTable(doc, 'btWhaleCta', 'btPixelCta');
-        this.addDataField(doc, 'btPixelCta', 'color', (field) => {
+        this.addDataField(doc, 'btWhaleCta', 'color', (field) => {
             field.nodeValue = ''; // Set color value
         });
+        this.renameBlockTypeHandle(doc, 'whale_cta', 'pixel_cta');
+        this.renameDataTable(doc, 'btWhaleCta', 'btPixelCta');
     }
 }
