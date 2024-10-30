7c5e6c7736158189332dcad1ce603b33c63169ea
Michele Locati <michele@locati.it>
2024-10-30 18:49:36 +0100

    Done with Html

--- src/Conversion/Converter/Block/Html.ts
+++ src/Conversion/Converter/Block/Html.ts
@@ -3,9 +3,12 @@ import Block from '../Block';
 
 export default class Html extends Block implements Converter
 {
-    // @ts-ignore: 'doc' is declared but its value is never read
-    // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
+        this.processconvertBlockTemplates(doc, 'html', {
+            pixel_test_bordered: {newTemplate: '', warning: 'The pixel_test_bordered custom template of the Html block is deprecated'},
+            pixel_test: {newTemplate: '', warning: 'The pixel_test custom template of the Html block is deprecated'},
+            'pixel_test-2': {newTemplate: '', warning: 'The pixel_test-2 custom template of the Html block is deprecated'},
+        }, warnings);
     }
 }
