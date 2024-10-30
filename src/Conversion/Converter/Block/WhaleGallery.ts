615eda73d30c966a366ec5230e7489c410ad3469
Michele Locati <michele@locati.it>
2024-10-30 15:13:15 +0100

    Add missing fields to btWhaleGallery

--- src/Conversion/Converter/Block/WhaleGallery.ts
+++ src/Conversion/Converter/Block/WhaleGallery.ts
@@ -5,6 +5,15 @@ export default class WhaleGallery extends Block implements Converter
 {
     public convert(doc: XMLDocument): void
     {
+        this.addDataField(doc, 'btWhaleGallery', 'columnsSm', (el) => el.nodeValue = '0');
+        this.addDataField(doc, 'btWhaleGallery', 'columnsMd', (el) => el.nodeValue = '0');
+        this.addDataField(doc, 'btWhaleGallery', 'columnsLg', (el) => el.nodeValue = '0');
+        this.addDataField(doc, 'btWhaleGallery', 'columnsXl', (el) => el.nodeValue = '0');
+        this.addDataField(doc, 'btWhaleGallery', 'columnsXxl', (el) => el.nodeValue = '0');
+        this.addDataField(doc, 'btWhaleGallery', 'gutter', (el) => el.nodeValue = '0');
+        this.addDataField(doc, 'btWhaleGallery', 'cropImage', (el) => el.nodeValue = '0');
+        this.addDataField(doc, 'btWhaleGallery', 'maxWidth', (el) => el.nodeValue = '0');
+        this.addDataField(doc, 'btWhaleGallery', 'maxHeight', (el) => el.nodeValue = '0');
         this.renameBlockTypeHandle(doc, 'whale_gallery', 'pixel_gallery');
         this.renameDataTable(doc, 'btWhaleGallery', 'btPixelGallery');
     }
