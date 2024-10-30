cd2a77247ddc563c0e944d77c809e75ac58a8294
Michele Locati <michele@locati.it>
2024-10-30 18:46:44 +0100

    Done with HorizontalRule

--- src/Conversion/Converter/Block/HorizontalRule.ts
+++ src/Conversion/Converter/Block/HorizontalRule.ts
@@ -3,9 +3,28 @@ import Block from '../Block';
 
 export default class HorizontalRule extends Block implements Converter
 {
-    // @ts-ignore: 'doc' is declared but its value is never read
-    // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
+        this.processconvertBlockTemplates(doc, 'horizontal_rule', {
+            //pixel
+            pixel_center: {newTemplate: 'pixel', newCustomClasses: 'divider:center icon:circle'},
+            pixel_center_border: {newTemplate: 'pixel', newCustomClasses: 'divider:center icon:circle divider:border'},
+            pixel_center_border_short: {newTemplate: 'pixel', newCustomClasses: 'divider:center icon:circle divider:sm divider:border'},
+            pixel_center_rounded: {newTemplate: 'pixel', newCustomClasses: 'divider:center icon:circle divider:rounded'},
+            pixel_center_rounded_short: {newTemplate: 'pixel', newCustomClasses: 'divider:center icon:circle divider:sm divider:rounded'},
+            pixel_center_short: {newTemplate: 'pixel', newCustomClasses: 'divider:center icon:circle divider:sm'},
+            pixel_left: {newTemplate: 'pixel', newCustomClasses: 'icon:circle'},
+            pixel_left_border: {newTemplate: 'pixel', newCustomClasses: 'divider:left icon:circle divider:border'},
+            pixel_left_border_short: {newTemplate: 'pixel', newCustomClasses: 'divider:left icon:circle divider:sm divider:border'},
+            pixel_left_rounded: {newTemplate: 'pixel', newCustomClasses: 'divider:left icon:circle divider:rounded'},
+            pixel_left_rounded_short: {newTemplate: 'pixel', newCustomClasses: 'divider:left icon:circle divider:sm divider:rounded'},
+            pixel_left_short: {newTemplate: 'pixel', newCustomClasses: 'divider:left icon:circle divider:sm'},
+            pixel_right: {newTemplate: 'pixel', newCustomClasses: 'divider:right icon:circle'},
+            pixel_right_border: {newTemplate: 'pixel', newCustomClasses: 'divider:right icon:circle divider:border'},
+            pixel_right_border_short: {newTemplate: 'pixel', newCustomClasses: 'divider:right icon:circle divider:sm divider:border'},
+            pixel_right_rounded: {newTemplate: 'pixel', newCustomClasses: 'divider:right icon:circle divider:rounded'},
+            pixel_right_rounded_short: {newTemplate: 'pixel', newCustomClasses: 'divider:right icon:circle divider:sm divider:rounded'},
+            pixel_right_short: {newTemplate: 'pixel', newCustomClasses: 'divider:right icon:circle divider:sm'},
+        }, warnings);
     }
 }
