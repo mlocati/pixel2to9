25de4b6671a9a55152409b810b3027e3d5b34ae8
Michele Locati <michele@locati.it>
2024-10-30 18:13:58 +0100

    Done with ExpressEntryDetail

--- src/Conversion/Converter/Block/ExpressEntryDetail.ts
+++ src/Conversion/Converter/Block/ExpressEntryDetail.ts
@@ -3,9 +3,13 @@ import Block from '../Block';
 
 export default class ExpressEntryDetail extends Block implements Converter
 {
-    // @ts-ignore: 'doc' is declared but its value is never read
-    // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
+        this.processconvertBlockTemplates(doc, 'express_entry_detail', {
+            pixel_team_member_grid: {newTemplate: '', warning: 'The pixel_team_member_grid custom template of the Express Entry Detail block is deprecated'},
+            pixel_team_member_list: {newTemplate: '', warning: 'The pixel_team_member_grid custom template of the Express Entry Detail block is deprecated'},
+            pixel_testimonial_grid: {newTemplate: '', warning: 'The pixel_team_member_grid custom template of the Express Entry Detail block is deprecated'},
+            pixel_testimonial_list: {newTemplate: '', warning: 'The pixel_team_member_grid custom template of the Express Entry Detail block is deprecated'},
+        }, warnings);
     }
 }
