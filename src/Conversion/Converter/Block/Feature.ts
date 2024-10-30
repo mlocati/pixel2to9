491275b0c7d226158a44c9a4256dfa32763ac0f5
Michele Locati <michele@locati.it>
2024-10-30 18:34:58 +0100

    Done with Feature

--- src/Conversion/Converter/Block/Feature.ts
+++ src/Conversion/Converter/Block/Feature.ts
@@ -3,9 +3,14 @@ import Block from '../Block';
 
 export default class Feature extends Block implements Converter
 {
-    // @ts-ignore: 'doc' is declared but its value is never read
-    // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
+        this.processconvertBlockTemplates(doc, 'feature', {
+            pixel_alert: {newTemplate: '', warning: 'The pixel_alert custom template of the Feature block is deprecated'},
+            pixel_alert_bs: {newTemplate: '', warning: 'The pixel_alert_bs custom template of the Feature block is deprecated'},
+            pixel_bordered: {newTemplate: 'pixel_feature_box', newCustomClasses: 'fbox:layout:bordered'},
+            pixel_centered_icon: {newTemplate: 'pixel_feature_box', newCustomClasses: 'fbox:layout:center'},
+            pixel_side_icon: {newTemplate: 'pixel_feature_box'},
+        }, warnings);
     }
 }
