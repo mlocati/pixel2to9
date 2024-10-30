24467151e96505b9274513346ae9deed3b29b08d
Michele Locati <michele@locati.it>
2024-10-30 18:39:13 +0100

    Done with File

--- src/Conversion/Converter/Block/File.ts
+++ src/Conversion/Converter/Block/File.ts
@@ -3,9 +3,13 @@ import Block from '../Block';
 
 export default class File extends Block implements Converter
 {
-    // @ts-ignore: 'doc' is declared but its value is never read
-    // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
+        this.processconvertBlockTemplates(doc, 'file', {
+            pixel: {newCustomClasses: 'promo:border buttom:style:3d fileinfo:show-type fileinfo:show-size'},
+            pixel_color: {newTemplate: 'pixel', newCustomClasses: 'promo-dark bg-color buttom:light fileinfo:show-type fileinfo:show-size'},
+            pixel_dark: {newTemplate: 'pixel', newCustomClasses: 'promo:dark fileinfo:show-type fileinfo:show-size'},
+            pixel_light: {newTemplate: 'pixel', newCustomClasses: 'promo:light buttom:dark fileinfo:show-type fileinfo:show-size'},
+        }, warnings);
     }
 }
