977729102ca09402a73c4540e3fb4091f8205c50
Michele Locati <michele@locati.it>
2024-10-30 18:30:36 +0100

    Done with Faq

--- src/Conversion/Converter/Block/Faq.ts
+++ src/Conversion/Converter/Block/Faq.ts
@@ -3,9 +3,13 @@ import Block from '../Block';
 
 export default class Faq extends Block implements Converter
 {
-    // @ts-ignore: 'doc' is declared but its value is never read
-    // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
+        this.processconvertBlockTemplates(doc, 'faq', {
+            pixel_1: {newTemplate: 'pixel_toggle'},
+            pixel_2: {newTemplate: '', warning: 'The pixel_2 custom template of the Faq block is deprecated'},
+            pixel_3: {newTemplate: '', warning: 'The pixel_3 custom template of the Faq block is deprecated'},
+            pixel_4_tabs: {newTemplate: '', warning: 'The pixel_4_tabs custom template of the Faq block is deprecated'},
+        }, warnings);
     }
 }
