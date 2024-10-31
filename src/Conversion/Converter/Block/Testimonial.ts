60e1613e98c6ea721f843573b1057aa4bd597634
Michele Locati <michele@locati.it>
2024-10-31 12:22:06 +0100

    Done with Testimonial

--- src/Conversion/Converter/Block/Testimonial.ts
+++ src/Conversion/Converter/Block/Testimonial.ts
@@ -3,9 +3,11 @@ import Block from '../Block';
 
 export default class Testimonial extends Block implements Converter
 {
-    // @ts-ignore: 'doc' is declared but its value is never read
-    // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
+        this.processconvertBlockTemplates(doc, 'testimonial', {
+            pixel_grid: {newTemplate: 'pixel'},
+            pixel_list: {newTemplate: 'pixel', newCustomClasses: 'layout:center'},
+        }, warnings);
     }
 }
