360a5ae761fd2c4f4565bfc314bca3be450926bd
Michele Locati <michele@locati.it>
2024-10-31 11:40:17 +0100

    Done with Next Previous

--- src/Conversion/Converter/Block/NextPrevious.ts
+++ src/Conversion/Converter/Block/NextPrevious.ts
@@ -3,9 +3,10 @@ import Block from '../Block';
 
 export default class NextPrevious extends Block implements Converter
 {
-    // @ts-ignore: 'doc' is declared but its value is never read
-    // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
+        this.processconvertBlockTemplates(doc, 'next_previous', {
+            pixel_post_nav: {newTemplate: '', warning: 'The pixel_post_nav custom template of the Next Previous block is deprecated'},
+        }, warnings);
     }
 }
