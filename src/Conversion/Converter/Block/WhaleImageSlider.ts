77ad6e22a2159a85e9886a7c688cff76b9753f95
Michele Locati <michele@locati.it>
2024-10-31 14:38:01 +0100

    Done with Whale Image Slider

--- src/Conversion/Converter/Block/WhaleImageSlider.ts
+++ src/Conversion/Converter/Block/WhaleImageSlider.ts
@@ -6,8 +6,15 @@ export default class WhaleImageSlider extends Block implements Converter
     // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
-        this.renameBlockTypeHandle(doc, 'whale_image_slider', 'pixel_slider');
+        this.removeDataFields(doc, 'btWhaleImageSlider', ['scroller', 'heightXss', 'heightXs']);
+        this.addDataField(doc, 'btWhaleImageSlider', 'heightXl', (field) => field.nodeValue = '0');
+        this.addDataField(doc, 'btWhaleImageSlider', 'slideNumbers', (field) => field.nodeValue = '0');
+        this.addDataField(doc, 'btWhaleImageSlider', 'autoplayOnce', (field) => field.nodeValue = '0');
+        this.addDataField(doc, 'btWhaleImageSlider', 'disableParallax', (field) => field.nodeValue = '0');
         this.renameDataTable(doc, 'btWhaleImageSlider', 'btPixelSlider');
+
         this.renameDataTable(doc, 'btWhaleImageSliderEntries', 'btPixelSliderEntries');
+
+        this.renameBlockTypeHandle(doc, 'whale_image_slider', 'pixel_slider');
     }
 }
