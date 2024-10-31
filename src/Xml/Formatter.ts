ba938c65d7b2fecc072e11a7e49befcf2f8ba496
Michele Locati <michele@locati.it>
2024-10-31 10:42:26 +0100

    Fix serializing XML fragment

--- src/Xml/Formatter.ts
+++ src/Xml/Formatter.ts
@@ -2,7 +2,7 @@ import xmlFormat from 'xml-formatter';
 
 const xmlSerializer = new XMLSerializer();
 
-function formatXML(el: Node): string
+function formatXML(el: Element): string
 {
   const xml = xmlSerializer.serializeToString(el);
   return xmlFormat(xml, {
@@ -17,7 +17,11 @@ export default function formatCIF(doc: XMLDocument, isFragment: boolean): string
 {
   if (isFragment) {
     const chunks: string[] = [];
-    doc.documentElement.childNodes.forEach((n) => chunks.push(formatXML(n)));
+    doc.documentElement.childNodes.forEach((n) => {
+      if (n.nodeType === Node.ELEMENT_NODE) {
+        chunks.push(formatXML(<Element>n));
+      }
+    });
     return chunks.join('\n');
   }
   return formatXML(doc);
