cf902e5e0b7744c3397d540655f681041783a2fd
Michele Locati <michele@locati.it>
2024-10-31 12:17:07 +0100

    Done with Social Links

--- src/Conversion/Converter/Block/SocialLinks.ts
+++ src/Conversion/Converter/Block/SocialLinks.ts
@@ -3,9 +3,11 @@ import Block from '../Block';
 
 export default class SocialLinks extends Block implements Converter
 {
-    // @ts-ignore: 'doc' is declared but its value is never read
-    // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
+        this.processconvertBlockTemplates(doc, 'social_links', {
+            pixel_footer_icons: {newTemplate: '', warning: 'The pixel_footer_icons custom template of the Social Links block is deprecated'},
+            pixel_top_bar: {newTemplate: '', warning: 'The pixel_top_bar custom template of the Social Links block is deprecated'},
+        }, warnings);
     }
 }
