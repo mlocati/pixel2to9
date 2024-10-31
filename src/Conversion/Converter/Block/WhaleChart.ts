f9046bb9a1b063cc4e30ca009a641f355aaaface
Michele Locati <michele@locati.it>
2024-10-31 13:56:17 +0100

    Done with Whale Chart

--- src/Conversion/Converter/Block/WhaleChart.ts
+++ src/Conversion/Converter/Block/WhaleChart.ts
@@ -3,7 +3,6 @@ import Block from '../Block';
 
 export default class WhaleChart extends Block implements Converter
 {
-    // @ts-ignore: 'doc' is declared but its value is never read
     // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
