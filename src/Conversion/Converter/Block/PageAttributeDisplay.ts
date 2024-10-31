d4d0b595ef673377598abc7322ed48d3fa88f4f3
Michele Locati <michele@locati.it>
2024-10-31 11:42:21 +0100

    Done with Page Attribute Display

--- src/Conversion/Converter/Block/PageAttributeDisplay.ts
+++ src/Conversion/Converter/Block/PageAttributeDisplay.ts
@@ -3,9 +3,10 @@ import Block from '../Block';
 
 export default class PageAttributeDisplay extends Block implements Converter
 {
-    // @ts-ignore: 'doc' is declared but its value is never read
-    // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
+        this.processconvertBlockTemplates(doc, 'page_attribute_display', {
+            pixel_image_responsive_full: {newTemplate: 'pixel_image_responsive'},
+        }, warnings);
     }
 }
