75894beab8490b44c179d36d1c9840e6d175816b
Michele Locati <michele@locati.it>
2024-10-31 12:11:14 +0100

    Done with Page Title

--- src/Conversion/Converter/Block/PageTitle.ts
+++ src/Conversion/Converter/Block/PageTitle.ts
@@ -3,9 +3,19 @@ import Block from '../Block';
 
 export default class PageTitle extends Block implements Converter
 {
-    // @ts-ignore: 'doc' is declared but its value is never read
-    // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
+        this.processconvertBlockTemplates(doc, 'page_title', {
+            pixel_blog_title: {newTemplate: 'pixel_blog_entry_title'},
+            pixel_fancy_title_bottom_border: {newTemplate: 'pixel', newCustomClasses: 'title:bottom-border'},
+            pixel_fancy_title_bottom_border_short: {newTemplate: 'pixel', newCustomClasses: 'title:bottom-border:short'},
+            pixel_fancy_title_center_aligned: {newTemplate: 'pixel', newCustomClasses: 'title:fancy-border:double title:align:center'},
+            pixel_fancy_title_emphasis: {newTemplate: '', warning: 'The pixel_fancy_title_emphasis custom template of the Page Title block is deprecated'},
+            pixel_fancy_title_left_aligned: {newTemplate: 'pixel', newCustomClasses: 'title:fancy-border:single'},
+            pixel_fancy_title_left_aligned_border_double: {newTemplate: 'pixel', newCustomClasses: 'title:fancy-border:double'},
+            pixel_fancy_title_left_border: {newTemplate: 'pixel', newCustomClasses: 'title:left-border'},
+            pixel_fancy_title_right_aligned: {newTemplate: 'pixel', newCustomClasses: 'title:fancy-border:double title:align:right'},
+            pixel_plain: {newTemplate: '', warning: 'The pixel_plain custom template of the Page Title block is deprecated'},
+        }, warnings);
     }
 }
