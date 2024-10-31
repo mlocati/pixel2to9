412c385133f49ff7313c78b1f1f73271ea91b8a7
Michele Locati <michele@locati.it>
2024-10-31 14:49:51 +0100

    Done with Whale Pricing Table

--- src/Conversion/Converter/Block/WhalePricingTable.ts
+++ src/Conversion/Converter/Block/WhalePricingTable.ts
@@ -3,9 +3,13 @@ import Block from '../Block';
 
 export default class WhalePricingTable extends Block implements Converter
 {
-    // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
+        this.processconvertBlockTemplates(doc, 'whale_pricing_table', {
+            pixel_horizontal: {deprecated: true},
+            pixel_light: {newTemplate: '', newCustomClasses: 'pricing:minimal'},
+        }, warnings);
+        this.removeDataField(doc, 'btWhalePricingTable', 'featured');
         this.renameBlockTypeHandle(doc, 'whale_pricing_table', 'pixel_pricing_table');
         this.renameDataTable(doc, 'btWhalePricingTable', 'btPixelPricingTable');
     }
