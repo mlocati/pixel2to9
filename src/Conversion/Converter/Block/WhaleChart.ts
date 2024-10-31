c020ababfbe24d3e67b0bb5586f4a7eb2b373cf3
Michele Locati <michele@locati.it>
2024-10-31 16:53:21 +0100

    Convert FontAwesome icons in Whale Chart

--- src/Conversion/Converter/Block/WhaleChart.ts
+++ src/Conversion/Converter/Block/WhaleChart.ts
@@ -3,9 +3,9 @@ import Block from '../Block';
 
 export default class WhaleChart extends Block implements Converter
 {
-    // @ts-ignore: 'warnings' is declared but its value is never read
     public convert(doc: XMLDocument, warnings: string[]): void
     {
+        this.convertFontAwesomeDataField(doc, 'btWhaleChart', 'icon', warnings);
         this.renameBlockTypeHandle(doc, 'whale_chart', 'pixel_pie_chart');
         this.removeDataFields(doc, 'btWhaleChart', ['lineCap', 'scaleColor']);
         this.renameDataTable(doc, 'btWhaleChart', 'btPixelPieChart');
